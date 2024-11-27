3369f5d26da8b078a9efa12a5e147fbfadfb0abb
LrnzZ <142222644+LrnzZ@users.noreply.github.com>
2024-11-27 20:50:58 +0800

    test

--- BorrowBooks.java
+++ BorrowBooks.java
@@ -60,7 +60,7 @@ public class BorrowBooks extends JFrame {
         backgroundPanel.add(scrollPane);
 
         // Borrow button
-        JButton borrowButton = new JButton("Borrow this book");
+        JButton borrowButton = new JButton("Borrow book");
         borrowButton.setFont(new Font("SansSerif", Font.BOLD, 23));
         borrowButton.setBackground(new Color(0x603F26));
         borrowButton.setForeground(Color.WHITE);
